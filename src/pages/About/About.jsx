80b1ec735f4968f6a2525b98dbbc7419026a6d4e
jsimpsonux <jsimpsonux@gmail.com>
2024-04-08 23:14:44 +0100

    Made changes to the About change and used different method

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
-import Image from 'react-bootstrap/Image';
-import Stack from 'react-bootstrap/Stack';
+import Button from "react-bootstrap/Button";
+import Image from "react-bootstrap/Image";
+import Stack from "react-bootstrap/Stack";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import "./style.css";
-
 
 function About() {
   return (
     <>
-    <div class="bg-dark bg-opacity-25 ">
-      <Stack gap={3} class="col-md-3 col-sm-4 col-lg-3 col-xl-3 col-xs-2 mx-auto">
-        <h1 class="p-4">Hi, I am Jack.</h1>
-        <div class="p-4">
-        <Image src="/images/Myself.png" id="selfimage" thumbnail height="auto" class="image-expand" />
-        </div>
-        <div class="p-4">
-        <Button href="/projects" variant="primary">Take a peek</Button>
-        </div>
-      </Stack>
-      </div>
-      <div class="">
-      </div>
+    <Container fluid className="bg-dark bg-opacity-25">
+      <Row className="align-items-center">
+          <Col md={{ span: 5, offset: 1 }}>
+            <p class="text-start fs-1">Hello, I'm Jack,
+            <br /> a friendly web developer. </p>
+          <Button href="/projects" variant="primary" className="float-start">
+            Take a peek
+          </Button>
+          </Col>
+      </Row>
+      {/* <div class=""></div> */}
+    </Container>
     </>
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,23 +6,26 @@ function Projects() {
     {
       title: "My Relaxing Planner",
       image: "/images/Food.jpg",
+      link: "https://github.com/jsimpsonux/my-relaxing-planner",
     },
     {
       title: "Ingredient Alchemy",
       image: "/images/Food.jpg",
+      link: "https://github.com/jsimpsonux/Ingredient-Alchemy",
     },
     {
-      title: "Console Finances",
+      title: "Crypto-Mock",
       image: "/images/Food.jpg",
+      link: "https://github.com/jsimpsonux/Console-finances",
     },
   ];
   return (
     <>
       <div>
-        <h1>My Projects</h1>
-        <div class="d-flex justify-content-center flex-lg-row flex-sm-column flex-xs-column">
+        <h1 class="p-5">My Projects</h1>
+        <div class="d-flex justify-content-center flex-lg-row flex-sm-column flex-xs-column p-5">
             {data.map((project) => {
-              return <Card image={project.image} title={project.title} />;
+              return <Card image={project.image} title={project.title} href={project.link} />;
             })}
         </div>
       </div>
