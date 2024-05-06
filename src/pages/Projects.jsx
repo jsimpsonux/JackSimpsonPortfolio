f4afc899f1dede3799fe414ade9cd8c800aa24d9
jsimpsonux <jsimpsonux@gmail.com>
2024-05-06 11:15:36 +0100

    Added description

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Card from "../components/Card";
-import Stack from 'react-bootstrap/Stack';
 import { Link } from "react-router-dom";
 
 function Projects() {
@@ -8,18 +7,21 @@ function Projects() {
     {
       title: "My Relaxing Planner",
       image: "src/assets/relax.png",
+      description: "A friendly planner that provides the user with a place to plan out their day, with helpful features for the user.",
       github: "https://github.com/jsimpsonux/my-relaxing-planner",
       link: "https://jsimpsonux.github.io/my-relaxing-planner/",
     },
     {
       title: "Ingredient Alchemy",
       image: "src/assets/ingredient.png",
+      description: "Ingredient Alchemy allows users to be as picky as they want and still find a recipe fit for them.",
       github: "https://github.com/jsimpsonux/Ingredient-Alchemy",
       link: "https://jsimpsonux.github.io/Ingredient-Alchemy/",
     },
     {
       title: "Crypto-Mock",
       image: "src/assets/crypto.png",
+      description: "A crypto website that simulates real trading with the market, and provides users with a friendly inuitive design.",
       github: "https://github.com/jsimpsonux/Crypto-Mock",
       link: "https://resplendent-gumption-b3f3fe.netlify.app/",
     },
@@ -34,6 +36,8 @@ function Projects() {
               <div className="p-4 text-start d-md-flex">
                 <div className="vstack gap-3">
                   <Card image={project.image} title={project.title} />
+                  <br />
+                  <p>{project.description}</p>
                   <br />
                   <Link to={project.github} className="text-decoration-none">Github</Link>
                   <br />
